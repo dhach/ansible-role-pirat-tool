13ff6a97b3fef4ba6641929074d02c58f36d9f2e
dhs <dominik.hach@gmail.com>
2020-04-17 20:48:21 +0200

    reactivated original tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -19,12 +19,12 @@ def test_pirat_images_present(host):
         image_present = host.run("docker image history {0}:stable".format(img))
         assert image_present.rc == 0
 
-# def test_pirat_service_running(host):
-#     pirat_Service = host.service('pirat')
-#     assert pirat_service.is_running
+def test_pirat_service_running(host):
+    pirat_Service = host.service('pirat')
+    assert pirat_service.is_running
 
-# def test_pirat_reachable(host):
-#     endpoints = [ 'http://localhost:8081', 'http://localhost:5000' ]
-#     for e in endpoints:
-#         http_curl = host.run('curl {0}'.format(e))
-#         assert http_curl.rc == 0
+def test_pirat_reachable(host):
+    endpoints = [ 'http://localhost:8081', 'http://localhost:5000' ]
+    for e in endpoints:
+        http_curl = host.run('curl {0}'.format(e))
+        assert http_curl.rc == 0
